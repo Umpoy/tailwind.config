c4a9cb45a1a4a55cc16a7ded03a1fcbabf933cfa
umpoy <iumpoy@yahoo.com>
2021-10-27 15:22:17 -0700

    added fnl tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    padding: {
+      sm: "20px",
+      md: "40px",
+      lg: "80px",
+    },
+    extend: {},
+    screens: {
+      mobile: "295px",
+      tablet: "426px",
+      laptop: "770px",
+      desktop: "992px",
+    },
+    height: {
+      v80: "80vh",
+    },
+    colors: {
+      fresh: "#00bf6f",
+      white: "#fff",
+      black: "#000",
+      protein: "#e20000",
+      proteinDark: "#a90000",
+      paleo: "#ce762f",
+      paleoDark: "#c05200",
+      keto: "#ffb500",
+      ketoDark: "#da8c00",
+      vegan: "#7a9560",
+      veganDark: "#425a31",
+      veganlc: "#6e9dd6",
+      veganlcDark: "#4b6aa5",
+      whole30: "#60b5c1",
+      whole30Dark: "#4c8f98",
+      whole30Light: "#dcecef",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
